24af196982a3cd68284e95d4a86fcdbf741a4ccb
VGFsirius <41200584+VGFsirius@users.noreply.github.com>
2024-04-17 23:16:05 -0400

    init
    first commit

--- MyJavaProject/MyClass.java
+++ MyJavaProject/MyClass.java
@@ -1,4 +1,13 @@
+/**
+ * This is a simple Java class that prints "Hello, world!" to the console.
+ */
 public class MyClass {
+
+    /**
+     * This is the main method of the class. It prints "Hello, world!" to the console.
+     *
+     * @param args Command-line arguments (not used in this program)
+     */
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
